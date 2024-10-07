6de892fe37b952d4783ca8f27fc728f711a99fe6
rarias1898 <rarias1898@ceuandalucia.es>
2024-10-07 20:32:26 +0200

    Ejercicios 18,19,20

---
+++ TEMA2/js/temporizadores.js
@@ -0,0 +1,59 @@
+// ! Ejercicio 18
+// var color = "blue"
+
+// const cambioColor = () => {
+//   if(color == "blue") {
+//     document.body.style.backgroundColor="orange"
+//     color = "orange"
+//   }else{
+//     document.body.style.backgroundColor="blue"
+//     color = "blue"
+//   }
+// }
+
+// let intervalo = setInterval(cambioColor, 1000)
+
+// const detenerColor = () => {
+//   clearInterval(intervalo)
+// }
+
+// ! Ejercicio 19
+// document.write("Nos vamos!")
+// const locTime = setTimeout(() => {
+//   window.open("https://www.google.es", "google", "")
+// }, 5000);
+
+// ! Ejercicio 20
+const donQui = setInterval(() => {
+  console.log("En un lugar de la Mancha")
+}, 2000);
+
+const pararDonq = () => {
+  clearInterval(donQui)
+}
+
+const hanPasado = () => {
+  const segundos = setTimeout(() => {
+    console.log("Han pasado 3 segundos")
+  }, 3000);
+}
+
+var ventana1;
+const openVentana = () => {
+  ventana1 = window.open("", "Ventana1", "width=400, height=400")
+  
+  let interval1 = setInterval(() => {
+    let timeNow = new Date().toLocaleTimeString()
+    ventana1.document.write(`<p>${timeNow}</p>`)
+
+  }, 1000);
+
+  setTimeout(() => {
+    clearInterval(interval1)
+  }, 5000)
+
+  setTimeout(() => {
+    ventana1.close()
+  }, 10000)
+}
+
