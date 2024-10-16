4910d3ac8105de207f863ae538d4de46b1d49c66
Ángel Arias <angadominguez96@gmail.com>
2024-10-16 22:19:41 +0200

    Terminado hasta el ej7

--- TEMA2/Arrays/arrays.js
+++ TEMA2/Arrays/arrays.js
@@ -1,21 +1,21 @@
 // ! Ejercicio 1
-const arr = [2, 3, 4, 5, 0]
+const arr = [2, 3, 4, 5, 0];
 
-let result = 0
+let result = 0;
 const suma = () => {
-  arr.forEach(element => {
-    result += element
-  })
-  return result
-}
+  arr.forEach((element) => {
+    result += element;
+  });
+  return result;
+};
 
 const triple = () => {
-  let triple = []
-  arr.forEach(element => {
-    triple.push(element * 3)
-  })
-  return triple
-}
+  let triple = [];
+  arr.forEach((element) => {
+    triple.push(element * 3);
+  });
+  return triple;
+};
 
 // console.log(suma())
 // console.log(result / arr.length)
@@ -40,46 +40,92 @@ const triple = () => {
 
 // ! Ejercicio 3
 const personas = [
-  { nombre: "Angel", edad: 28, ciudad: "Sevilla"},
-  { nombre: "Angel", edad: 27, ciudad: "Huelva"},
-  { nombre: "Angel", edad: 26, ciudad: "Cadiz"},
-  { nombre: "Angel", edad: 25, ciudad: "Sevilla"},
-  { nombre: "Angel", edad: 12, ciudad: "Sevilla"},
-]
+  { nombre: "Angel", edad: 28, ciudad: "Sevilla" },
+  { nombre: "Angel", edad: 27, ciudad: "Huelva" },
+  { nombre: "Angel", edad: 26, ciudad: "Cadiz" },
+  { nombre: "Angel", edad: 25, ciudad: "Sevilla" },
+  { nombre: "Angel", edad: 12, ciudad: "Sevilla" },
+];
 
 const mayoresDeEdad = (per) => {
-  const mayores = per.filter((e) => e.edad >= 18 )
-  return mayores
-}
+  const mayores = per.filter((e) => e.edad >= 18);
+  return mayores;
+};
 
 const deSevilla = (per) => {
-  const sevillanos = per.filter((e) => e.ciudad == "Sevilla" )
-  return sevillanos
-}
+  const sevillanos = per.filter((e) => e.ciudad == "Sevilla");
+  return sevillanos;
+};
 
 // console.log(mayoresDeEdad(personas))
 // console.log(deSevilla(personas))
 
 // ! Ejercicio 4
 const alumnos = [
-  { nombre: "mesa", precio: 10, categoria: "moviliario"  },
+  { nombre: "mesa", precio: 10, categoria: "moviliario" },
   { nombre: "silla", precio: 7, categoria: "moviliario" },
   { nombre: "cuadro", precio: 5, categoria: "decoracion" },
   { nombre: "ordenador", precio: 60, categoria: "informatica" },
   { nombre: "teclado", precio: 21, categoria: "informatica" },
-]
+];
 
 const soloProductos = (productos) => {
-  const arrN = []
+  const arrN = [];
   productos.forEach((e) => {
-    arrN.push(e.nombre)
-  })
+    arrN.push(e.nombre);
+  });
   // console.log(arrN)
   // return arrN
-  return document.write(arrN.join(" - "))
-}
+  return document.write(arrN.join(" - "));
+};
 
 // console.log(soloProductos(alumnos))
 
 // ! Ejercicio 5
-// const palabra = prompt("Introduce una palabra")
+const contarVocales = () => {
+  const palabra = prompt("Introduce una palabra").toLowerCase().split("");
+  let contador = 0;
+  palabra.forEach((e) => {
+    if (e == "a" || e == "e" || e == "i" || e == "o" || e == "u") {
+      contador++;
+    }
+  });
+  return `La palabra ${palabra.join("")} tiene ${contador} vocales`;
+};
+
+// console.log(contarVocales())
+
+// ! Ejercicio 6
+const diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+
+const longPalabra = (array) => {
+  array.forEach((e) => {
+    console.log(`La palabra ${e} tiene ${e.length} letras`);
+  });
+}
+
+const masLarga = (array) => {
+  let palabraLarga = "";
+  array.forEach((e) => {
+    if (e.length > palabraLarga.length) {
+      palabraLarga = e;
+    }
+  });
+  return `La palabra ${palabraLarga} es la más larga`;
+}
+
+// longPalabra(diasSemana)
+// console.log(masLarga(diasSemana))
+
+// ! Ejercicio 7
+const invertirPalabra = () => {
+  let palabra = "";
+  do {
+    palabra = prompt("Introduce una palabra");
+  } while (palabra == "");
+
+  let invertida = palabra.split("").reverse().join("");
+  return `La palabra ${palabra} invertida es ${invertida}`;
+};
+
+// console.log(invertirPalabra())
