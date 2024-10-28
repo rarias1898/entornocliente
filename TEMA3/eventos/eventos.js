2ec39dd09e6a006a548ebfc64b8c9e9e203b4470
rarias1898 <rarias1898@ceuandalucia.es>
2024-10-28 20:45:36 +0100

    continuamos en los ejercicios de eventos

--- TEMA3/eventos/eventos.js
+++ TEMA3/eventos/eventos.js
@@ -35,3 +35,51 @@ const cambiameNaranja = (e) => {
 // document.getElementById('button1').addEventListener("click", pulsame)
 // document.getElementById('button2').addEventListener("mouseover", cambiame)
 // document.getElementById('button2').addEventListener("mouseout", cambiameNaranja)
+
+// ! Ejercicio 3
+const validar = () => {
+  let ej3Checkbox = document.getElementsByName("ej3")
+  let counter = 0
+
+  for (let i = 0; i < ej3Checkbox.length; i++) {
+    if (ej3Checkbox[i].checked) {
+      counter++
+    }
+  }
+
+  if (counter === 3) {
+    alert("Has seleccionado 3 o más checkbox")
+  }
+
+}
+
+// ! Ejercicio 4
+// let numero = Number(document.getElementById("numero").innerText)
+
+// const sumar = () => {
+//   numero++
+//   document.getElementById("numero").innerText = numero
+// }
+
+// const restar = () => {
+//   numero--
+//   document.getElementById("numero").innerText = numero
+
+//   if (numero == 3) {
+//     document.getElementById("numero").innerText += " Alcanzado valor 3"
+//   }
+// }
+
+// ! Ejercicio 5
+const form = document.createElement("form")
+
+for (let i = 0; i < 9; i++) {
+  const button = document.createElement("button")
+  button.setAttribute("class", "boton")
+
+  form.appendChild(button)
+}
+
+document.body.appendChild(form)
+
+const botones = document.getElementById("boton").addEventListener("click",)
