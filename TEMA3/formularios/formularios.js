6e3232662651652dfde94e5434e222654de85ae1
rarias1898 <rarias1898@ceuandalucia.es>
2024-11-06 18:33:04 +0100

    TErminado ej11 de formularios

--- TEMA3/formularios/formularios.js
+++ TEMA3/formularios/formularios.js
@@ -10,7 +10,7 @@
 // ! Ejercicio 2
 // document.getElementById('boton1').addEventListener('click', (e) => {
 //   const nombreUsuario = prompt("¿Cuál es tu nombre?");
-  
+
 //   if (nombreUsuario) {
 //       document.getElementById('input1').value = nombreUsuario;
 //       e.target.value = "Pulsado"
@@ -24,7 +24,7 @@
 
 // ! Ejercicio 4
 // document.getElementById("botonMarcar2").addEventListener("click", (e) => {
-  
+
 //   let suma = 0
 
 //   for (let i = 0; i < 4; i++) {
@@ -62,22 +62,86 @@
 // });
 
 // ! Ejercicio 6
-document.getElementById("miForm").addEventListener("submit", (e) => {
+// document.getElementById("miForm").addEventListener("submit", (e) => {
+//   e.preventDefault();
+
+//   const radios = document.getElementsByName("rad1");
+//   let seleccion = null;
+
+//   for (let radio of radios) {
+//       if (radio.checked) {
+//           seleccion = radio.value;
+//           break;
+//       }
+//   }
+
+//   if (seleccion) {
+//       alert("La opción seleccionada es: " + seleccion);
+//   } else {
+//       alert("No se ha seleccionado ninguna opción.");
+//   }
+// });
+
+// ! Ejercicio 9
+// document.getElementById("boton1").addEventListener("click", (e) => {
+//     e.preventDefault()
+
+//     let texto = document.getElementById("name")
+//     let datos = document.getElementById("datos")
+
+//     datos.innerText = texto.value
+//     datos.setAttribute("class", "resultado")
+
+//     texto.value = ''
+//     texto.focus()
+// })
+
+// ! Ejercicio 10
+// document.getElementById("enviarDatos").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let select = document.getElementById("prov");
+
+//   let opciones = select.options;
+
+//   alert(
+//     select.selectedIndex +
+//       " - " +
+//       select.value +
+//       " - " +
+//       opciones[select.selectedIndex].text
+//   );
+// });
+
+// document.getElementById("enviarDatos2").addEventListener("click", (e) => {
+//   e.preventDefault();
+
+//   let select = document.getElementById("prov2");
+//   let opciones = select.options;
+
+//     for (let i = 0; i < opciones.length; i++) {
+//       if(select[i].selected) {
+//         console.log(select.text)
+//       }
+//     }
+// });
+
+// ! Ejercicio 11
+document.getElementById("mayorDeEdad").addEventListener("click", (e) => {
   e.preventDefault();
 
-  const radios = document.getElementsByName("rad1");
-  let seleccion = null;
+  let edad = document.getElementById("edad").value;
+  let resultado = document.getElementById("resultado");
 
-  for (let radio of radios) {
-      if (radio.checked) {
-          seleccion = radio.value;
-          break;
-      }
-  }
-
-  if (seleccion) {
-      alert("La opción seleccionada es: " + seleccion);
+  if (!isNaN(edad)) {
+    if (edad >= 18) {
+      resultado.innerText = "Es mayor de edad";
+      resultado.setAttribute("class", "verde")
+    } else {
+      resultado.innerText = "Es menor de edad";
+      resultado.setAttribute("class", "rojo")
+    }
   } else {
-      alert("No se ha seleccionado ninguna opción.");
+    resultado.innerText = "No es un numero";
+    resultado.setAttribute("class", "rojo")
   }
 });
