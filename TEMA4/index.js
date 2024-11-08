a8a06f858a87757e2acfd860d6d348a945e158e2
Ángel Arias <angadominguez96@gmail.com>
2024-11-08 01:13:09 +0100

    Terminados ej del 1 al 5

---
+++ TEMA4/index.js
@@ -0,0 +1,25 @@
+// ! Ejercicio 1
+// const myDiv = document.getElementById("myDIV")
+// myDiv.firstElementChild.setAttribute("style", "background-color: yellow; color: red;")
+// myDiv.lastElementChild.setAttribute("style", "background-color: orange; color: red;")
+
+// ! Ejercicio 2 y 3
+// const div1 = document.getElementById("div1")
+// const ref = document.getElementById("p1")
+
+// const newP = document.createElement('p')
+// const newNewP = document.createElement('p')
+
+// newP.id = "p3"
+// newP.textContent = "Soy el nuevo"
+// div1.insertBefore(newP, ref)
+
+// newNewP.textContent = "Soy el primero ahora"
+// div1.insertBefore(newNewP, newP)
+
+// ! Ejercicio 4
+// const elemP = document.getElementsByTagName("p")[0]
+// document.body.removeChild(elemP)
+
+// ! Ejercicio 5
+const elemP = document.getElementsByTagName("p")[0].textContent = "reemplazado"
