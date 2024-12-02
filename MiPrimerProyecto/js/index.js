cba72b1280a3e14a1eb13376af171996e2f975e1
aaridoms <angadominguez96@gmail.com>
2024-12-02 01:25:16 +0100

    Borrado proyecto inicial

--- MiPrimerProyecto/js/index.js
+++
@@ -1,9 +0,0 @@
-// alert("¡Bienvenido a mi página web!")
-console.log("¡Hola, mundo!")
-document.write("<p>Me encanta esta asignatura.</p>")
-document.write("<a href='https://www.google.es'>Google</a>")
-
-let texto = "javascript \nes \n\ttremendo \nJohn's computer \n La version de \"Javascript\" es ES6" 
-
-console.log(texto)
-
