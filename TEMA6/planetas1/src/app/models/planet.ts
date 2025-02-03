6504bc6087ab0ee10b7fccd73612deac6a62f89b
rarias1898 <rarias1898@ceuandalucia.es>
2025-02-03 18:44:12 +0100

    Empezado ejercicio de los planetas

---
+++ TEMA6/planetas1/src/app/models/planet.ts
@@ -0,0 +1,10 @@
+export interface Planet {
+  name: string,
+  rotation_period: number,
+  orbital_period: number,
+  diameter: number,
+  gravity: string,
+  terrain: string,
+  surface_water: number,
+  population: number
+}
