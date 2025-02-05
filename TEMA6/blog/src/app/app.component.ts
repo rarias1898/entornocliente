fd4f219d580f9a9af00738e02fa054b9c91ca046
rarias1898 <rarias1898@ceuandalucia.es>
2025-02-05 17:49:43 +0100

    Terminado primer ejercicio de rutas

--- TEMA6/blog/src/app/app.component.ts
+++ TEMA6/blog/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blog';
 }
--- TEMA6/blog/src/app/app.routes.ts
+++ TEMA6/blog/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '/home' }
+];
