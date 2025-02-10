92d3decc835d9d45a0eea1ba1eb30a293480f3c9
rarias1898 <rarias1898@ceuandalucia.es>
2025-02-10 18:56:50 +0100

    Cambios en lso dos ultimos ej

--- TEMA6/biblioteca/src/app/app.routes.ts
+++ TEMA6/biblioteca/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LibrosComponent } from './libros/libros.component';
 import { LibroDetalleComponent } from './libro-detalle/libro-detalle.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
   { path: '',  component: HomeComponent },
   { path: 'home',  component: HomeComponent },
   { path: 'libros', component: LibrosComponent },
   { path: 'detalles/:id', component: LibroDetalleComponent },
+  { path: 'contacto', component: ContactoComponent },
   { path: '**', redirectTo: '/home' }
 ];
